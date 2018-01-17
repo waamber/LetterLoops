c78e416d6a2546fd7bd93d3b7cd6156f8a74c800
waamber <amberastuart@aol.com>
2018-01-17 17:24:07 -0600

    setup class

---
+++ LetterLoops/Inputs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterLoops
+{
+    class Inputs
+    {
+        public string AlphaString { get; set; }
+    }
+}
